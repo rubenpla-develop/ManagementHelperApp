cfabf330fa32612b9b15811d576c20e916bc8451
Ruben Pla <ruben.pla@alten.es>
2017-10-24 15:13:21 +0200

    - [LAYOUT] Switching from LinearLayout to FrameLayout Root element - [FRAGMENT] Basic structure for minimum opertativity

--- app/src/main/java/bcn/alten/altenappmanagement/ui/fragment/FollowUpFragment.java
+++ app/src/main/java/bcn/alten/altenappmanagement/ui/fragment/FollowUpFragment.java
@@ -1,14 +1,59 @@
 package bcn.alten.altenappmanagement.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thoughtbot.expandablerecyclerview.models.ExpandableList;
 
 import java.util.List;
 
+import bcn.alten.altenappmanagement.R;
+import bcn.alten.altenappmanagement.mvp.presenter.FollowUpFragmentPresenter;
 import bcn.alten.altenappmanagement.mvp.view.IFollowUpFragmentView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FollowUpFragment extends Fragment implements IFollowUpFragmentView {
 
-    public FollowUpFragment(){};
+    private final String TAG = FollowUpFragment.class.getSimpleName();
+
+    @BindView(R.id.followup_recyclerView)
+    private ExpandableList expandableList;
+
+    private FollowUpFragmentPresenter presenter;
+
+    public static FollowUpFragment newInstance() {
+            Bundle args = new Bundle();
+
+            //arguments
+            FollowUpFragment fragment = new FollowUpFragment();
+            if (!args.isEmpty()) {
+                fragment.setArguments(args);
+            }
+
+            return fragment;
+    }
+
+    public FollowUpFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_follow_up,
+                container, false);
+
+        ButterKnife.bind(this, view);
+
+        presenter = new FollowUpFragmentPresenter(this);
+
+        return view;
+    }
 
     @Override
     public void ShowFollowUpList(List<Object> list) {
