ba8d95ba14a6c68a5fb1a3e5e7742abc7310a5c0
Ruben Pla <ruben.pla@alten.es>
2017-10-24 17:33:34 +0200

    - [DRAWABLE] State drawable selector for BottomNavigaitonView - [ViewPager] Viewpager combined with BottomNavigationView, you can swipe or click on Navigation bar

--- app/src/main/java/bcn/alten/altenappmanagement/AltenAppMainActivity.java
+++ app/src/main/java/bcn/alten/altenappmanagement/AltenAppMainActivity.java
@@ -4,21 +4,31 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import bcn.alten.altenappmanagement.adapter.ViewPagerAdapter;
 import bcn.alten.altenappmanagement.mvp.view.IMainActivityView;
+import bcn.alten.altenappmanagement.ui.fragment.FollowUpFragment;
+import bcn.alten.altenappmanagement.ui.fragment.HomeFragment;
+import bcn.alten.altenappmanagement.ui.fragment.QMFragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AltenAppMainActivity extends AppCompatActivity implements IMainActivityView{
+public class AltenAppMainActivity extends AppCompatActivity implements IMainActivityView,
+        ViewPager.OnPageChangeListener {
 
-    @BindView(R.id.message)
-    TextView mTextMessage;
+    private final String TAG = AltenAppMainActivity.class.getSimpleName();
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
 
     @BindView(R.id.navigation)
     BottomNavigationView bottomNavigationView;
+
+    private ViewPagerAdapter adapter;
+    private MenuItem prevMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,24 @@ public class AltenAppMainActivity extends AppCompatActivity implements IMainActi
         setContentView(R.layout.activity_alten_app_main);
         ButterKnife.bind(this);
 
+        setupViewPager();
+
+        viewPager.addOnPageChangeListener(this);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void setupViewPager() {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        HomeFragment homeFragment = new HomeFragment();
+        FollowUpFragment followupFragment = new FollowUpFragment();
+        QMFragment QMFragment = new QMFragment();
+
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(followupFragment);
+        adapter.addFragment(QMFragment);
+
+        viewPager.setAdapter(adapter);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -35,20 +62,44 @@ public class AltenAppMainActivity extends AppCompatActivity implements IMainActi
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    viewPager.setCurrentItem(0);
+                    return true;
                 case R.id.navigation_follow_up:
-                    mTextMessage.setText(R.string.title_follow_up);
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_qm:
-                    mTextMessage.setText(R.string.title_qm);
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
         }
     };
 
-
     @Override
     public void showMessage(String message) {
         Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (prevMenuItem != null) {
+            prevMenuItem.setChecked(false);
+        } else {
+            bottomNavigationView.getMenu().getItem(0).setChecked(false);
+        }
+
+        bottomNavigationView.getMenu().getItem(position).setChecked(true);
+        prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
