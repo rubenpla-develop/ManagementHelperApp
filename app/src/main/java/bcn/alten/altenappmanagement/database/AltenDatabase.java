95004dedd697c15bd64d30031aae1ba56630841b
Ruben Pla <ruben.pla@alten.es>
2017-11-15 14:40:55 +0100

    - Migration implemented

--- app/src/main/java/bcn/alten/altenappmanagement/database/AltenDatabase.java
+++ app/src/main/java/bcn/alten/altenappmanagement/database/AltenDatabase.java
@@ -1,9 +1,12 @@
 package bcn.alten.altenappmanagement.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.VisibleForTesting;
 
 import bcn.alten.altenappmanagement.mvp.model.FollowUp;
 
@@ -13,11 +16,29 @@ public abstract class AltenDatabase extends RoomDatabase {
     private static AltenDatabase INSTANCE;
     private static final Object LOCK = new Object();
 
+    /**
+     * Migrate from:
+     * version 2 - using Room
+     * to
+     * version 3 - using Room where the {@link FollowUp} has an extra field: status
+     */
+    @VisibleForTesting
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE " + DbKeys.DATABASE_NAME
+                    + " ADD COLUMN status STRING");
+        }
+    };
+
     public static AltenDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
             synchronized (LOCK) {
                 INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                        AltenDatabase.class, DbKeys.DATABASE_NAME).build();
+
+                        AltenDatabase.class, DbKeys.DATABASE_NAME)
+                        .addMigrations(MIGRATION_1_2)
+                        .build();
             }
         }
 
@@ -25,4 +46,5 @@ public abstract class AltenDatabase extends RoomDatabase {
     }
 
     public abstract DaoAccess daoAccess();
+
 }
