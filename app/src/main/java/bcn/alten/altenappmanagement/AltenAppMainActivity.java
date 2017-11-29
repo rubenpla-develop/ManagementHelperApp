1763dc47a803520510f861de251287bbe5634d7c
Ruben Pla <ruben.pla@alten.es>
2017-11-29 17:01:23 +0100

    - [TOOLBAR] custom toolbar added to app - [ACTIVITY] Activity QMCreateEditActivity added, handles all about  creation&editin of QM items - [GRADLE] Updated Gradle build tools

--- app/src/main/java/bcn/alten/altenappmanagement/AltenAppMainActivity.java
+++ app/src/main/java/bcn/alten/altenappmanagement/AltenAppMainActivity.java
@@ -1,11 +1,15 @@
 package bcn.alten.altenappmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import bcn.alten.altenappmanagement.adapter.ViewPagerAdapter;
@@ -20,6 +24,9 @@ public class AltenAppMainActivity extends AppCompatActivity implements IMainActi
         ViewPager.OnPageChangeListener {
 
     private final String TAG = AltenAppMainActivity.class.getSimpleName();
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @BindView(R.id.viewpager)
     ViewPager viewPager;
@@ -36,8 +43,33 @@ public class AltenAppMainActivity extends AppCompatActivity implements IMainActi
         setContentView(R.layout.activity_alten_app_main);
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
         setupViewPager();
         setListeners();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.menu_toolbar_main_about:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void setupViewPager() {
